6aaa82dc44f92a8dde08055e774db48ef23d08ea
Claudia Shao <zxshao99@gmail.com>
2020-04-30 16:58:22 -0400

    revised drawing 2

--- drawing2/script.js
+++ drawing2/script.js
@@ -3,7 +3,9 @@ const sky = document.querySelector('.sky');
 const cloud1 = document.querySelector('.cloud1');
 const cloud2 = document.querySelector('.cloud2');
 const indicator = document.querySelector('.hover-indicator');
-const bg = document.querySelector('.clouds');
+const bg = document.querySelector('.rain');
+const clouds = document.querySelector('.clouds');
+const chair = document.querySelectorAll('.chair');
 
 x1 = 700;
 x2 = 800;
@@ -21,7 +23,7 @@ function getTime(){
 
 //change color based on current minute and second
 function colorChange(hours){
-    let opacity = Math.abs(12-hours)/10 - 0.5;
+    let opacity = Math.abs(12-hours)/10 - 0.4;
     filter.style.opacity = opacity;
     filter.style.transition = "1s ease";
 }
@@ -55,12 +57,20 @@ function blendmode(){
     cloud1.style.mixBlendMode = "hard-light";
     cloud2.style.mixBlendMode = "hard-light";
     bg.style.opacity = 1;
+    clouds.style.opacity = 1;
+    chair[0].style.opacity = 0;
+    chair[1].style.opacity = 0;
+    chair[2].style.opacity = 0;
 
 }
 function normal(){
     cloud1.style.mixBlendMode = "normal";
     cloud2.style.mixBlendMode = "normal";
     bg.style.opacity = 0;
+    clouds.style.opacity = 0;
+    chair[0].style.opacity = 1;
+    chair[1].style.opacity = 1;
+    chair[2].style.opacity = 1;
 }
 
 
