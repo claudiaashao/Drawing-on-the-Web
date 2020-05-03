1f9870d79d95cddb05aa3c4e281f3394e40032e3
Claudia Shao <zxshao99@gmail.com>
2020-05-03 19:43:29 -0400

    linked drawing 3 audio

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const cactus = document.querySelector('.cactus');
 
 const recordAudio = document.querySelector('.record-audio');
 const planeAudio = document.querySelector('.plane-audio');
+const cactusAudio = document.querySelector('.cactus-audio');
 
 function recordAudioPlay(){
     recordAudio.play();
@@ -21,7 +22,18 @@ function planeAudioPause(){
     planeAudio.pause();
 }
 
+function cactusAudioPlay(){
+    cactusAudio.play();
+}
+
+function cactusAudioPause(){
+    cactusAudio.pause();
+}
+
 record.addEventListener("mouseover", recordAudioPlay);
 record.addEventListener("mouseout", recordAudioPause);
 plane.addEventListener("mouseover", planeAudioPlay);
 plane.addEventListener("mouseout", planeAudioPause);
+
+cactus.addEventListener("mouseover", cactusAudioPlay);
+cactus.addEventListener("mouseout", cactusAudioPause);
